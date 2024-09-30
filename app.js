096f3d3a695f307d439baeb62cb46780a692139a
Kartik014 <kartikagarwal014@gmail.com>
2024-09-30 19:09:33 +0530

    Implemented helmet, rate limitting

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import xssClean from "xss-clean";
 
 const app = express();
 
 
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
+
+const limiter = rateLimit({
+    windowMs : 15*60*1000,
+    max : 100,
+    message : "Too many request, Please try again later"
+})
+
+app.use(limiter)
+app.use(xssClean())
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: [
+              "'self'",
+              "'strict-dynamic'",
+            ],
+            styleSrc: [
+              "'self'",
+              "trusted-styles.com",
+              "'sha256-<your-hash>'",
+            ],
+            objectSrc: ["'none'"],
+            frameSrc: ["'none'"],
+            frameAncestors: ["'none'"],
+            baseUri: ["'self'"],
+            formAction: ["'self'"],
+            upgradeInsecureRequests: [],
+            blockAllMixedContent: []
+        },
+        crossOriginEmbedderPolicy: false,
+        crossOriginOpenerPolicy: "same-origin",
+        referrerPolicy: { policy: "no-referrer" },
+        dnsPrefetchControl: { allow: false },
+        frameguard: { action: 'deny' },
+        hidePoweredBy: true,
+        hsts: {
+            maxAge: 31536000,
+            includeSubDomains: true,
+            preload: true,
+        },
+        ieNoOpen: true,
+        noSniff: true,
+        xssFilter: true,
+    })
+  )  
 
 export default app;
